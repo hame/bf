e7eef59a8815f10a8b3a30c96551559718cefaf8
hame <h@hamishosborne.com>
2023-01-21 21:08:01 +1100

    Increment and decrement instructions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bf
-Brainf*ck
+Brainf*ck programming, but I'll refer to it as bf
+
+## Links
+Nice tutorial on the language:
+https://www.deusinmachina.net/p/weird-ones-30-years-of-brainfuck
+
+Nice video on writing an interpreter:
+https://www.youtube.com/watch?v=4uNM73pfJn0 
+
+My go to:
+https://esolangs.org/wiki/Brainfuck 
+
---
+++ interpreter.js
@@ -0,0 +1,34 @@
+// Interpreter for Brainfuck
+
+// Original implementation uses 30,000 memory cells
+// Scaling that back to 100 for ease of console.log
+let tape = new Array(100).fill(0); // Initialise all to 0
+
+let tape_pointer = 0;
+let instruction_pointer = 0;
+
+let source = "++";
+
+console.log(source.length);
+
+while (instruction_pointer < source.length) {
+  let instruction = source.charAt(instruction_pointer);
+  switch (instruction) {
+    case "+":
+      tape[tape_pointer]++;
+      if (tape[tape_pointer] == 256) {
+        tape[tape_pointer] = 0;
+      }
+      break;
+    case "-":
+      tape[tape_pointer]--;
+      if (tape[tape_pointer] == -1) {
+        tape[tape_pointer] = 255;
+      }
+      break;
+  }
+
+  instruction_pointer++;
+}
+
+console.log(tape);
