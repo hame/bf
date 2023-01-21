05775b918e9fca268b13965e5331dd27b4256f61
hame <h@hamishosborne.com>
2023-01-21 23:21:58 +1100

    I/O commands although I deferred

--- interpreter.js
+++ interpreter.js
@@ -4,7 +4,7 @@
 // Scaling that back to 100 for ease of console.log
 let tape = new Array(100).fill(0); // Initialise all to 0
 
-let tape_pointer = 0;
+let memory_pointer = 0;
 let instruction_pointer = 0;
 
 let source = "++>>++";
@@ -15,22 +15,28 @@ while (instruction_pointer < source.length) {
   let instruction = source.charAt(instruction_pointer);
   switch (instruction) {
     case "+":
-      tape[tape_pointer]++;
-      if (tape[tape_pointer] == 256) {
-        tape[tape_pointer] = 0;
+      tape[memory_pointer]++;
+      if (tape[memory_pointer] == 256) {
+        tape[memory_pointer] = 0;
       }
       break;
     case "-":
-      tape[tape_pointer]--;
-      if (tape[tape_pointer] == -1) {
-        tape[tape_pointer] = 255;
+      tape[memory_pointer]--;
+      if (tape[memory_pointer] == -1) {
+        tape[memory_pointer] = 255;
       }
       break;
     case ">":
-      tape_pointer++;
+      memory_pointer++;
       break;
     case "<":
-      tape_pointer--;
+      memory_pointer--;
+      break;
+    case ".":
+      console.log(tape[memory_pointer]);
+      break;
+    case ",":
+      // not implementing user input yet
       break;
   }
 
